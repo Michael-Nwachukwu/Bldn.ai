38e54604f5f0e59b9b459da18ed6e5e53bd2514a
Michael Nwachukwu <dahformulah@gmail.com>
2023-12-22 22:59:13 +0100

    color fixes and addition of market stats on crypty

--- src/components/crypty/Home.jsx
+++ src/components/crypty/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Spacer, Card, CardBody, VStack, Grid, GridItem,  useColorMode, useColorModeValue, List, ListItem , Image} from '@chakra-ui/react'
+import { Box, Flex, Text, Spacer, Card, CardBody, VStack, Grid, GridItem,  useColorMode, useColorModeValue, List, ListItem , Image, Divider} from '@chakra-ui/react'
 import React from 'react'
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
         <>
             <Box>
                 <Flex>
-                    <Box>
+                    <Box pt={2}>
                         <Text>Bitcoin BTC</Text>
                         <Text
                             fontSize={50}
@@ -21,46 +21,62 @@ const Home = () => {
                         </Text>
                     </Box>
                     <Spacer />
-                    <Flex alignItems={"center"} gap={3}>
-                        <Card size={'sm'} variant={'outline'} bg={'transparent'} shadow={'sm'} border={''} borderColor={borderColor} borderRadius={10} outline={'brand.300'}>
-                            <CardBody>
+                    <Flex direction={'column'} alignItems={'end'} gap={2}>
+                        <Flex alignItems={'center'} gap={3}>
+                            <small style={{ color:'#dfe5ed' }}>
+                                Cryptos: <span style={{ color:'#edb3c3' }}>2M+</span>
+                            </small>
+                            <small style={{ color:'#dfe5ed' }}>
+                                Exchanges: <span style={{ color:'#edb3c3' }}>687</span>
+                            </small>
+                            <small style={{ color:'#dfe5ed' }}>
+                                Dominance: <span style={{ color:'#edb3c3' }}>BTC: 51.4% ETH: 16.8%</span>
+                            </small>
+                            <small style={{ color:'#dfe5ed' }}>
+                                ETH GAS: <span style={{ color:'#edb3c3' }}>52 GWEI </span>
+                            </small>
+                        </Flex>
+                        <Flex alignItems={"center"} gap={3}>
+                            <Card size={'sm'} variant={'outline'} bg={'transparent'} shadow={'sm'} border={''} borderColor={borderColor} borderRadius={10} outline={'brand.300'}>
+                                <CardBody>
+                                    <VStack align={'start'}>
+                                        <Text fontSize={'lg'} fontWeight={'extrabold'} color={'#dfe5ed'}>
+                                            $1,655,669,299,690
+                                        </Text>
+                                        <Text fontSize={'sm'} fontWeight={'light'} color={'#91a2b8'} >
+                                            Market Capitalization 
+                                        </Text>
+                                    </VStack>
+                                </CardBody>
+                            </Card>
+                            <Card size={'sm'} variant={'outline'} bg={'transparent'} shadow={'sm'} border={''} borderColor={borderColor} borderRadius={10} outline={'brand.300'}>
+                                <CardBody >
                                 <VStack align={'start'}>
-                                    <Text fontSize={'lg'} fontWeight={'extrabold'} color={'gray-200'}>
-                                        $1,655,669,299,690
-                                    </Text>
-                                    <Text fontSize={'sm'} >
-                                        Market Capitalization 
-                                    </Text>
-                                </VStack>
-                            </CardBody>
-                        </Card>
-                        <Card size={'sm'} variant={'outline'} bg={'transparent'} shadow={'sm'} border={''} borderColor={borderColor} borderRadius={10} outline={'brand.300'}>
-                            <CardBody >
-                            <VStack align={'start'}>
-                                    <Text fontSize={'lg'} fontWeight={'extrabold'} color={'gray-200'}>
-                                    $54,691,566,748
-                                    </Text>
-                                    <Text fontSize={'sm'} >
-                                        24h Trading Volume
-                                    </Text>
-                                </VStack>
-                            </CardBody>
-                        </Card>
+                                        <Text fontSize={'lg'} fontWeight={'extrabold'} color={'#dfe5ed'}>
+                                        $54,691,566,748
+                                        </Text>
+                                        <Text fontSize={'sm'} fontWeight={'light'} color={'#91a2b8'} >
+                                            24h Trading Volume
+                                        </Text>
+                                    </VStack>
+                                </CardBody>
+                            </Card>
+                        </Flex>
                     </Flex>
                 </Flex>
             </Box>
 
             <Grid templateColumns='repeat(3, 1fr)' gap={6} mt={6}>
                 <GridItem w='100%'>
-                    <Card size={'sm'} h={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
+                    <Card size={'sm'} h={'auto'} minH={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
                         <CardBody>
                             <VStack>
                                 <Flex alignItems={'center'} w={'100%'}>
                                     <Text fontSize={18} fontWeight={'semibold'} color={colorMode === "light" ? 'gray.600' : 'white'}>🔥 Trending</Text>
                                     <Spacer />
-                                    <Box w={4} h={4} borderRadius={'full'} bg={colorMode === "light" ? 'gray.700' : 'white'}></Box>
+                                    <Box w={4} h={4} borderRadius={'full'} bg={colorMode === "light" ? 'gray.700' : '#dfe5ed'}></Box>
                                 </Flex>
-                                <List w={'100%'} spacing={6} mt={2} color={color}>
+                                <List w={'100%'} spacing={3} mt={2} color={color}>
                                     <ListItem>
                                         <Flex alignItems={'center'}>
                                             <Flex alignItems={'center'} gap={4}>
@@ -78,6 +94,7 @@ const Home = () => {
                                             <Text>1.56568341</Text>
                                         </Flex>
                                     </ListItem>
+                                    <Divider />
                                     <ListItem>
                                         <Flex alignItems={'center'}>
                                             <Flex alignItems={'center'} gap={4}>
@@ -95,6 +112,7 @@ const Home = () => {
                                             <Text>1.56568341</Text>
                                         </Flex>
                                     </ListItem>
+                                    <Divider />
                                     <ListItem>
                                         <Flex alignItems={'center'}>
                                             <Flex alignItems={'center'} gap={4}>
@@ -118,14 +136,14 @@ const Home = () => {
                     </Card>
                 </GridItem>
                 <GridItem w='100%'>
-                    <Card size={'sm'} h={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
+                    <Card size={'sm'} h={'auto'} minH={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
                         <CardBody>
 
                         </CardBody>
                     </Card>
                 </GridItem>
                 <GridItem w='100%'>
-                    <Card size={'sm'} h={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
+                    <Card size={'sm'} h={'auto'} minH={48} variant={'outline'} bg={'transparent'} borderRadius={10} shadow={'md'} border={'1px'} borderColor={borderColor} outline={'brand.300'}>
                         <CardBody>
                             
                         </CardBody>
