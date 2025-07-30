136cb2d9a22e64a6cd2b2c098f806b6bf85fe087
“Michael <dahformulah@gmail.com>
2025-07-30 13:49:08 +0100

    Enhance PWA support by updating favicon links, adding new icons, and modifying Vite configuration for web app manifest. Include new favicon files and an apple touch icon.

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,16 @@ export default defineConfig({
       start_url: '/',
       icons: [
         {
-          src: 'b.png',
-          sizes: '192x192',
-          type: 'image/png'
+          "src": "/web-app-manifest-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
         },
         {
-          src: 'outlook-green.png',
-          sizes: '512x512',
-          type: 'image/png'
+          "src": "/web-app-manifest-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
         }
       ]
     }
